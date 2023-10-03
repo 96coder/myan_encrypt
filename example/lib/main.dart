09ec32695b87092577b166d4972d00e7500a1de0
96coder <mmcoder96@gmail.com>
2023-10-03 13:46:50 +0630

    update

---
+++ example/lib/main.dart
@@ -0,0 +1,100 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:myan_encrypt/myan_encrypt.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Flutter String Encryptor')),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                TextButton(
+                  onPressed: () {
+                    NormalEncrypt normalEncrypt =
+                        NormalEncrypt('_encryptionKey');
+                    log(normalEncrypt.encrypt('plainText'));
+                  },
+                  child: const Text('Normal Encrypt'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    NormalEncrypt normalEncrypt =
+                        NormalEncrypt('_encryptionKey');
+                    log(normalEncrypt
+                        .decrypt(normalEncrypt.encrypt('plainText')));
+                  },
+                  child: const Text('Normal Decrypt'),
+                ),
+              ],
+            ),
+            const Divider(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                TextButton(
+                  onPressed: () {
+                    EncryptUtil encryptUtil =
+                        EncryptUtil('_encryptionKey', '_hmacKey');
+                    log(encryptUtil.encrypt('plainText'));
+                  },
+                  child: const Text('Encrypt I'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    EncryptUtil encryptUtil =
+                        EncryptUtil('_encryptionKey', '_hmacKey');
+                    log(encryptUtil.decrypt(encryptUtil.encrypt('plainText')));
+                  },
+                  child: const Text('Decrypt I'),
+                ),
+              ],
+            ),
+            const Divider(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                TextButton(
+                  onPressed: () {
+                    EncryptUtil2 encryptUtil2 = EncryptUtil2('encryptionKey');
+                    log(encryptUtil2.encrypt('plainText'));
+                  },
+                  child: const Text('Encrypt II'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    EncryptUtil2 encryptUtil2 = EncryptUtil2('encryptionKey');
+                    log(encryptUtil2
+                        .decrypt(encryptUtil2.encrypt('plainText')));
+                  },
+                  child: const Text('Decrypt II'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
